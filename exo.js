f64828f7c8db01641eaca675291c61e2bf06a4b6
Amaranth01 <vanessa.nectoux@gmail.com>
2021-11-25 15:24:30 +0100

    Exo 82 ok

---
+++ exo.js
@@ -0,0 +1,106 @@
+/*
+1
+ */
+let value1 = 34.589456;
+value1 = Math.floor(value1);
+console.log(value1);
+
+/*
+2
+ */
+
+let value2 = 68.412784;
+value2 = Math.floor(value2);
+console.log(value2);
+
+/*
+3
+ */
+
+let value3 = 39.456;
+value3 = Math.ceil(value3);
+console.log(value3);
+
+/*
+4
+ */
+
+let value4 = 27.34876541;
+value4  = Math.ceil(value4);
+console.log(value4);
+
+/*
+5
+ */
+
+let value5 = 5.68;
+value5 = Math.round(value5);
+console.log(value5);
+
+/*
+6
+ */
+
+let value6 = 5.41 ;
+value6 = Math.round(value6);
+console.log(value6);
+
+/*
+7
+ */
+
+let value7 = 6.12457;
+value7 = Math.trunc(value7);
+console.log(value7);
+
+/*
+8
+ */
+
+let value8 = 9.568124;
+value8 = Math.trunc(value8);
+console.log(value8);
+
+/*
+9
+ */
+
+let a = 5, b = 1, c = 12, d = -1, e = 45;
+    let  min1 = Math.min(a, b, c, d, e);
+    console.log(min1);
+ /*
+ 10
+  */
+
+let f = 9, g = 12, h = 16, i = 44, j=3;
+    let min = Math.min(f, g, h, i, j);
+    console.log(min);
+/*
+11
+ */
+
+let k = 5, l = 1, m = -1, n = 45;
+    let max = Math.max(j, k, l, m, n);
+console.log(max);
+
+/*
+12
+ */
+
+let o = 9, p = 12, q = 16, r = 44, s = 3;
+let max2 =Math.max(o, p, q, r, s);
+console.log(max2);
+
+/*
+13
+ */
+
+let absolute = Math.abs(-6);
+console.log(absolute);
+
+/*
+14
+ */
+
+let abs = Math.abs(-56);
+console.log(abs);
